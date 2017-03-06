a9ca038d90bbc0189fea21ac95b9227e3c5e9f4b
Nicolas Jourdain <nicolas.jourdain@univ-grenoble-alpes.fr>
2017-03-06 22:23:10 +0100

    add bdy tools

---
+++ concatenate_yearly_BDY.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CONFIG="WED12"
+BDY_DIR="$SHAREDELMER/input/nemo_WED12/BDY"
+YEARi=1993
+YEARf=2013
+
+for BDY in bdyT_tra bdyU_u3d bdyU_u2d bdyV_u3d bdyV_u2d bdyT_ice bdyT_ssh
+do
+
+for YEAR in $(seq $YEARi $YEARf)
+do
+
+  ncrcat ${BDY_DIR}/${BDY}_${YEAR}_??_${CONFIG}.nc ${BDY_DIR}/${BDY}_${YEAR}_${CONFIG}.nc
+  if [ -f ${BDY_DIR}/${BDY}_${YEAR}_${CONFIG}.nc ]; then
+    rm -f ${BDY_DIR}/${BDY}_${YEAR}_??_${CONFIG}.nc
+    echo "${BDY_DIR}/${BDY}_${YEAR}_${CONFIG}.nc  [oK]"
+  else
+    echo "~!@#%^&* ERROR: ${BDY_DIR}/${BDY}_${YEAR}_${CONFIG}.nc HAS NOT BEEN CREATED   >>>>> STOP !!"
+    exit
+  fi
+
+done
+
+done
