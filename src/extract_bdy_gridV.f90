4fa578b0ffa89900b70a711ee630ca91fba14a74
Nicolas Jourdain <nicolas.jourdain@univ-grenoble-alpes.fr>
2017-04-05 10:41:29 +0200

    add correction to conserve transport in case of slight gird stretching between parent and child domain

--- src/extract_bdy_gridV.f90
+++ src/extract_bdy_gridV.f90
@@ -20,16 +20,16 @@ IMPLICIT NONE
 namelist /general/ config, config_dir
 namelist /bdy_data/ nn_yeari, nn_yearf, data_dir, data_prefix, nn_bdy_eosmatch, &
 &                   data_suffix_T, data_suffix_S, data_suffix_U, data_suffix_V, &
-&                   data_suffix_ssh, data_suffix_ice, file_data_mask, file_data_zgr
+&                   data_suffix_ssh, data_suffix_ice, file_data_mask, file_data_zgr, file_data_hgr
 
 CHARACTER(LEN=50)                    :: config
 CHARACTER(LEN=150)                   :: config_dir, data_dir, data_prefix, data_suffix_T, data_suffix_S, &
 &                                       data_suffix_U, data_suffix_V, data_suffix_ssh, data_suffix_ice,  &
-&                                       file_data_mask, file_data_zgr
+&                                       file_data_mask, file_data_zgr, file_data_hgr
 INTEGER                              :: nn_yeari, nn_yearf, nn_bdy_eosmatch
 
 INTEGER                              :: fidCOORD, status, dimID_yb, dimID_xbv,                       &
-&                                       myb, mxbv, glamv_ID, gphiv_ID, e1v_ID, e2v_ID,               &
+&                                       myb, mxbv, glamv_ID, gphiv_ID, e1v_ID, e2v_ID, e1v_GLO_ID,   &
 &                                       nbiv_ID, nbjv_ID, nbrv_ID, mtime, dimID_x, dimID_y,          &
 &                                       mlon, mlat, mdepthv, kday, kmonth, kyear, kbdy, nfmt, fidN,  &
 &                                       kt, kz,     lon_ID, lat_ID, depthv_ID, vobtcrty_ID, fidM,    &
@@ -37,14 +37,14 @@ INTEGER                              :: fidCOORD, status, dimID_yb, dimID_xbv,  
 &                                       dimID_time_counter, dimID_depthv, time_ID, dimID_time, fidS, &
 &                                       i, j, k, l, fidC, imin_ORCA12, jmin_ORCA12, iGLO, jGLO,      &
 &                                       depth_ID, ai, aj, bi, bj, kfmt, fidMSKIN, vmask_GLO_ID,      &
-&                                       e3v_GLO_ID, fidZGRIN
+&                                       e3v_GLO_ID, fidZGRIN, fidHGRIN
 CHARACTER(LEN=100)                   :: calendar, time_units
 CHARACTER(LEN=150)                   :: file_coord, file_in_gridV, file_bdy_gridV2d,  &
 &                                       file_in_coord_REG, command_str, file_bdy_gridV3d
 INTEGER*4,ALLOCATABLE,DIMENSION(:)   :: list_fmt
 INTEGER*4,ALLOCATABLE,DIMENSION(:,:) :: nbiv, nbjv, nbrv
 INTEGER*1,ALLOCATABLE,DIMENSION(:,:,:) :: vmask_GLO
-REAL*4,ALLOCATABLE,DIMENSION(:,:)    :: glamv, gphiv, e1v, e2v, nav_lon, nav_lat, nav_lon_bdy, nav_lat_bdy
+REAL*4,ALLOCATABLE,DIMENSION(:,:)    :: e1v_GLO, glamv, gphiv, e1v, e2v, nav_lon, nav_lat, nav_lon_bdy, nav_lat_bdy
 REAL*4,ALLOCATABLE,DIMENSION(:,:,:)  :: e3v_GLO, vobtcrty, vobtcrty_bdy
 REAL*4,ALLOCATABLE,DIMENSION(:,:,:,:):: vomecrty, vomecrty_bdy
 REAL*4,ALLOCATABLE,DIMENSION(:)      :: depthv
@@ -266,7 +266,7 @@ status = NF90_INQ_VARID(fidMSKIN,"vmask",vmask_GLO_ID); call erreur(status,.TRUE
 status = NF90_GET_VAR(fidMSKIN,vmask_GLO_ID,vmask_GLO); call erreur(status,.TRUE.,"getvar_vmask_GLO")
 status = NF90_CLOSE(fidMSKIN);                          call erreur(status,.TRUE.,"end read mask_GLO")
 
-!- Read e3v and e3v in large-scale/global file:
+!- Read e1v and e3v in large-scale/global file:
 status = NF90_OPEN(TRIM(file_data_zgr),0,fidZGRIN);     call erreur(status,.TRUE.,"read mask input")
 ALLOCATE(  e3v_GLO(mlon,mlat,mdepthv)  )
 status = NF90_INQ_VARID(fidZGRIN,"e3v",e3v_GLO_ID) 
@@ -274,6 +274,12 @@ if ( status .ne. 0 ) status = NF90_INQ_VARID(fidZGRIN,"e3v_0",e3v_GLO_ID)
 call erreur(status,.TRUE.,"inq_e3v_GLO_ID")
 status = NF90_GET_VAR(fidZGRIN,e3v_GLO_ID,e3v_GLO);     call erreur(status,.TRUE.,"getvar_e3v_GLO")
 status = NF90_CLOSE(fidZGRIN);                          call erreur(status,.TRUE.,"end read mask_GLO")
+!-
+status = NF90_OPEN(TRIM(file_data_hgr),0,fidHGRIN);     call erreur(status,.TRUE.,"read mask input")
+ALLOCATE(  e1v_GLO(mlon,mlat)  )
+status = NF90_INQ_VARID(fidHGRIN,"e1v",e1v_GLO_ID);     call erreur(status,.TRUE.,"inq_e1v_GLO_ID")
+status = NF90_GET_VAR(fidHGRIN,e1v_GLO_ID,e1v_GLO);     call erreur(status,.TRUE.,"getvar_e1v_GLO")
+status = NF90_CLOSE(fidHGRIN);                          call erreur(status,.TRUE.,"end read mask_GLO")
 
 !--
 
@@ -383,8 +389,6 @@ DO kyear=nn_yeari,nn_yearf
         
         status = NF90_CLOSE(fidV)                                     ; call erreur(status,.TRUE.,"fin_lecture")     
 
-        write(*,*) '@@1 ', minval(vomecrty)
-
         !---------------------------------------
         ! Remove possible NaNs :
  
@@ -399,8 +403,6 @@ DO kyear=nn_yeari,nn_yearf
         enddo
         enddo
         enddo
-
-        write(*,*) '@@2 ', minval(vomecrty)
 
         !---------------------------------------
         ! Calculate the barotropic component :
@@ -430,11 +432,11 @@ DO kyear=nn_yeari,nn_yearf
         enddo
         enddo
 
-        write(*,*) '@@3 ', minval(vomecrty)
-
         !---------------------------------------
         ! Fill values on bdyV :
-      
+        ! (we ensure transport conservation in case grids are 
+        ! slightly stretched with respect to each other)     
+ 
         ALLOCATE( vomecrty_bdy(mxbv,1,mdepthv,mtime)  )
         ALLOCATE( vobtcrty_bdy(mxbv,1,mtime)  )
 
@@ -444,13 +446,11 @@ DO kyear=nn_yeari,nn_yearf
           write(*,*) kbdy, iGLO, jGLO, nbiv(kbdy,1), nbjv(kbdy,1)
           do kt=1,mtime
           do kz=1,mdepthv
-            vomecrty_bdy(kbdy,1,kz,kt) = vomecrty( iGLO, jGLO, kz, kt )
-            vobtcrty_bdy(kbdy,1,   kt) = vobtcrty( iGLO, jGLO,     kt )
+            vomecrty_bdy(kbdy,1,kz,kt) = vomecrty( iGLO, jGLO, kz, kt ) * e1v_GLO( iGLO, jGLO ) / (e1v(kbdy,1)*ai)
+            vobtcrty_bdy(kbdy,1,   kt) = vobtcrty( iGLO, jGLO,     kt ) * e1v_GLO( iGLO, jGLO ) / (e1v(kbdy,1)*ai)
           enddo
           enddo
         enddo
-
-        write(*,*) '@@4 ', minval(vomecrty_bdy)
 
         !--------------------------------------
         ! Write BDY netcdf file for barotropic velocity :
